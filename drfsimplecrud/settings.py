9385daad9fdfc8f383ebbb45d73fda106e5cbebf
Brayan Salamanca <salamancaayalabrayandavid@gmail.com>
2025-08-21 15:54:40 +0000

    Cambiar renderers para usar Browsable API primero

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -128,14 +128,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Configuración de Django Rest Framework
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
-        "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework.renderers.JSONRenderer",
     ]
 }
 
 
+
 # CORS configuración
-CORS_ALLOW_ALL_ORIGINS = True  # ⚠️ solo para pruebas, en prod usa lista segura
+CORS_ALLOW_ALL_ORIGINS = True  
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
 #     "http://127.0.0.1:3000",
